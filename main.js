a9db73beb688b088ec91a18cdaf9a6b898f8ce66
Christian Aranas <aranaschristianlouise@gmail.com>
2024-02-11 23:52:18 +0800

    Initial implementation of routing
    Added router.js to implement SPA-alike routing.
    All servable static files are moved to /static folder.
    Updated references (main.js, all webc files, and others) to the files that are moved to /static folder.
    Updated references in taileind.config.js.
    All links in `nav-bar.webc` is updated to have relative hrefs, which `router.js` will handle.

--- main.js
+++ main.js
@@ -1,14 +1,27 @@
 import { WebC } from "@11ty/webc";
 import fs from "fs";
+const COMPONENTS_DIR = "./components/**.webc";
 
 // Compiling index.html
-let page = new WebC();
-const COMPONENTS_DIR = "./components/**.webc";
-page.defineComponents(COMPONENTS_DIR);
-page.setInputPath("./page.webc");
+let index = new WebC();
+index.defineComponents(COMPONENTS_DIR);
+index.setInputPath("./pages/index.webc");
 
-page.compile().then(({ html, css, js, components }) => {
+index.compile().then(({ html, css, js, components }) => {
   fs.writeFile("./index.html", html, (err) => {
+    if (err) {
+      console.log({ err });
+    }
+  });
+});
+
+// Compiling home.html
+let home = new WebC();
+home.defineComponents(COMPONENTS_DIR);
+home.setInputPath("./pages/home.webc");
+
+home.compile().then(({ html, css, js, components }) => {
+  fs.writeFile("./static/home.html", html, (err) => {
     if (err) {
       console.log({ err });
     }
@@ -21,7 +34,7 @@ library.defineComponents(COMPONENTS_DIR);
 library.setInputPath("./pages/library.webc");
 
 library.compile().then(({ html, css, js, components }) => {
-  fs.writeFile("./library.html", html, (err) => {
+  fs.writeFile("./static/library.html", html, (err) => {
     if (err) {
       console.log({ err });
     }
@@ -34,7 +47,7 @@ about.defineComponents(COMPONENTS_DIR);
 about.setInputPath("./pages/about.webc");
 
 about.compile().then(({ html, css, js, components }) => {
-  fs.writeFile("./about.html", html, (err) => {
+  fs.writeFile("./static/about.html", html, (err) => {
     if (err) {
       console.log({ err });
     }
