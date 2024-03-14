b404b9bd79065c005dc6752eafade8d005ddf177
Christian Aranas <aranaschristianlouise@gmail.com>
2024-03-14 18:31:05 +0800

    Cleaned `main.js` WebC usage
    `main.js` now only uses one instance of WebC object.

--- main.js
+++ main.js
@@ -9,15 +9,14 @@ function isDev() {
 const getUrl = (url = "/") => {
   return `${isDev() ? "" : "/tailwind-infinix-website"}${url}`;
 };
+let webc = new WebC();
+webc.setHelper("getUrl", getUrl);
+webc.defineComponents(COMPONENTS_DIR);
 
 // Compiling index.html
 console.log("Compiling index.html");
-let index = new WebC();
-index.setHelper("getUrl", getUrl);
-index.defineComponents(COMPONENTS_DIR);
-index.setInputPath("./pages/index.webc");
-
-index.compile().then(({ html, css, js, components }) => {
+webc.setInputPath("./pages/index.webc");
+webc.compile().then(({ html, css, js, components }) => {
   fs.writeFile("./index.html", html, (err) => {
     if (err) {
       console.log({ err });
@@ -27,12 +26,8 @@ index.compile().then(({ html, css, js, components }) => {
 
 // Compiling home.html
 console.log("Compiling home.html");
-let home = new WebC();
-home.setHelper("getUrl", getUrl);
-home.defineComponents(COMPONENTS_DIR);
-home.setInputPath("./pages/home.webc");
-
-home.compile().then(({ html, css, js, components }) => {
+webc.setInputPath("./pages/home.webc");
+webc.compile().then(({ html, css, js, components }) => {
   fs.writeFile("./static/home.html", html, (err) => {
     if (err) {
       console.log({ err });
@@ -42,12 +37,8 @@ home.compile().then(({ html, css, js, components }) => {
 
 // Compiling library.html
 console.log("Compiling library.html");
-let library = new WebC();
-library.setHelper("getUrl", getUrl);
-library.defineComponents(COMPONENTS_DIR);
-library.setInputPath("./pages/library.webc");
-
-library.compile().then(({ html, css, js, components }) => {
+webc.setInputPath("./pages/library.webc");
+webc.compile().then(({ html, css, js, components }) => {
   fs.writeFile("./static/library.html", html, (err) => {
     if (err) {
       console.log({ err });
@@ -56,13 +47,9 @@ library.compile().then(({ html, css, js, components }) => {
 });
 
 // Compiling about.html
-let about = new WebC();
 console.log("Compiling about.html");
-about.setHelper("getUrl", getUrl);
-about.defineComponents(COMPONENTS_DIR);
-about.setInputPath("./pages/about.webc");
-
-about.compile().then(({ html, css, js, components }) => {
+webc.setInputPath("./pages/about.webc");
+webc.compile().then(({ html, css, js, components }) => {
   fs.writeFile("./static/about.html", html, (err) => {
     if (err) {
       console.log({ err });
